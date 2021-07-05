d8574e2889afc3f24f28a36a72c809e8425ce888
Juyoung Lee <ejuyoung@gmail.com>
2021-07-05 23:52:08 +0900

    remove quaternion to euler part

--- buds_to_unity.py
+++ buds_to_unity.py
@@ -17,28 +17,6 @@ from SpatialSensorManager import SpatialSensorManager
 SENDING_STR = "0.00,0.00,0.00,0.00"
 import numpy as np
 
-def quaternion_to_euler_angle_vectorized1(w, x, y, z):
-    ysqr = y * y
-
-    t0 = +2.0 * (w * x + y * z)
-    t1 = +1.0 - 2.0 * (x * x + ysqr)
-    X = np.degrees(np.arctan2(t0, t1))
-
-    t2 = +2.0 * (w * y - z * x)
-    t2 = np.where(t2>+1.0,+1.0,t2)
-    #t2 = +1.0 if t2 > +1.0 else t2
-
-    t2 = np.where(t2<-1.0, -1.0, t2)
-    #t2 = -1.0 if t2 < -1.0 else t2
-    Y = np.degrees(np.arcsin(t2))
-
-    t3 = +2.0 * (w * z + x * y)
-    t4 = +1.0 - 2.0 * (ysqr + z * z)
-    Z = np.degrees(np.arctan2(t3, t4))
-
-    return X, Y, Z 
-
-
 def __spatial_sensor_callback(quaternion):
     global SENDING_STR
     # This parameter is a float list describing a raw quaternion (4D vector)
